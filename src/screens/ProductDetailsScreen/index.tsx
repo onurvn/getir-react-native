de22e91459b62d0a849fe90dad8b035df0595963
Onur Avan <avanonur@hotmail.com>
2025-05-24 00:03:44 +0300

    Add Button component and ProductDetail component to ProductDetailsScreen

--- src/screens/ProductDetailsScreen/index.tsx
+++ src/screens/ProductDetailsScreen/index.tsx
@@ -1,11 +1,44 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, ScrollView, Text, View } from "react-native";
+import { Product } from "../../models";
+import ImageCarousel from "../../components/imageCarousel";
+import DetailBox from "../../components/detailBox";
+import ProductDetail from "../../components/productDetail";
+import Button from "../../components/button";
 
-function index() {
+function index(props) {
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    setProduct(props.route.params.product);
+  }, []);
+
+  if (!product) {
+    return <ActivityIndicator color={"#5D3EBD"} />;
+  }
+
   return (
-    <View>
-      <Text>onur</Text>
-    </View>
+      <ScrollView>
+        <ImageCarousel images={product.images || []} />
+        <DetailBox
+          price={product.price}
+          name={product.name}
+          amount={product.amount}
+        />
+        <Text
+          style={{
+            paddingHorizontal: 10,
+            paddingVertical: 13,
+            color: "#808B99",
+            fontWeight: "600",
+            fontSize: 14,
+          }}
+        >
+          Detaylar
+        </Text>
+        <ProductDetail />
+        <Button />
+      </ScrollView>
   );
 }
 
