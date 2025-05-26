c1cfda00b32902b891fd68c44b54f37a121a4e63
Onur Avan <avanonur@hotmail.com>
2025-05-26 18:54:36 +0300

    Implement Redux for cart management with actions, reducers, and constants

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,9 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 
 import { composeWithDevTools } from "redux-devtools-extension";
+import cartItems from "./reducers/cartItem";
 
 const reducers = combineReducers({
-  
+  cartItems: cartItems,
 });
 
 const store = createStore(
