0930e27ddde6857c0e5efbf6f8c54266f56c21e0
Onur Avan <avanonur@hotmail.com>
2025-05-26 18:28:55 +0300

    Set up Redux store with thunk middleware and integrate Redux DevTools

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import RootNavigator from "./src/navigation/RootNavigator"
+import RootNavigator from "./src/navigation/RootNavigator";
+import store from "./src/redux/store";
+import { Provider } from "react-redux";
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
