8423a8c988fceaf3b65f25b8ebd3127ae07119a2
Onur Avan <avanonur@hotmail.com>
2025-05-26 18:03:09 +0300

    Add CartItem component and CartScreen for managing cart items

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -3,17 +3,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import CategoryFilterScreen from "../screens/CategoryFilterScreen";
 import ProductDetailsScreen from "../screens/ProductDetailsScreen";
-import { Image, Text, TouchableOpacity } from "react-native";
+import CartScreen from "../screens/CartScreen";
+import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
 import { Foundation, Ionicons } from "@expo/vector-icons";
 import {
   getFocusedRouteNameFromRoute,
   useNavigation,
 } from "@react-navigation/native";
 
+const { width, height } = Dimensions.get("window");
+
 const Stack = createStackNavigator();
 
 function MyStack({ navigation, route }) {
-  const tabHiddenRoutes = ["ProductDetails"];
+  const tabHiddenRoutes = ["ProductDetails", "CartScreen"];
 
   React.useLayoutEffect(() => {
     const routeName = getFocusedRouteNameFromRoute(route);
@@ -24,7 +27,6 @@ function MyStack({ navigation, route }) {
     }
   }, [navigation, route]);
   return (
-    
     <Stack.Navigator>
       <Stack.Screen
         name="Home"
@@ -51,10 +53,55 @@ function MyStack({ navigation, route }) {
               Ürünler
             </Text>
           ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("CartScreen")}
+              style={{
+                width: width * 0.2,
+                height: 30,
+                backgroundColor: "white",
+                marginRight: width * 0.03,
+                borderRadius: 9,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                style={{ width: 25, height: 25, marginLeft: 5 }}
+                source={require("../../assets/cart.png")}
+              />
+              <View
+                style={{ height: 30, width: 4, backgroundColor: "white" }}
+              />
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: "#F3EFFE",
+                  height: 30,
+                  borderTopRightRadius: 10,
+                  borderBottomRightRadius: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    color: "#5D3EBD",
+                    fontWeight: "bold",
+                    fontSize: 12,
+                  }}
+                >
+                  <Text>{"\u20BA"}</Text>24
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ),
         }}
       />
 
       <Stack.Screen
+        name="ProductDetails"
+        component={ProductDetailsScreen}
         options={{
           headerTintColor: "white",
           headerStyle: { backgroundColor: "#5C3EBC" },
@@ -74,8 +121,43 @@ function MyStack({ navigation, route }) {
             </Text>
           ),
         }}
-        name="ProductDetails"
-        component={ProductDetailsScreen}
+      />
+
+      <Stack.Screen
+        name="CartScreen"
+        component={CartScreen}
+        options={{
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: "#5C3EBC" },
+          headerTitle: () => (
+            <Text style={{ fontWeight: "bold", fontSize: 15, color: "white" }}>
+              Sepetim
+            </Text>
+          ),
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{ paddingLeft: 8 }}
+            >
+              <Ionicons
+                style={{ marginLeft: 4 }}
+                name="close"
+                size={26}
+                color="white"
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity style={{ paddingRight: 10 }}>
+              <Ionicons
+                style={{ marginRight: 8 }}
+                name="trash"
+                size={24}
+                color="white"
+              />
+            </TouchableOpacity>
+          ),
+        }}
       />
     </Stack.Navigator>
   );
---
+++ src/screens/CartScreen/index.tsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import Products from "../../../assets/data/products";
+import { Product } from "../../models";
+import ProductItem from "../../components/productItem";
+import CartItem from "../../components/cartItem";
+
+const { height, width } = Dimensions.get("window");
+
+function index({
+  cartItems,
+  route,
+}: {
+  cartItems: { product: Product; quantity: number }[];
+  route: any;
+}) {
+  const {
+    params: { message },
+  } = route;
+
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const getProductsPrice = () => {
+    let total = 0;
+    cartItems.forEach((product) => {
+      total += product.product.price;
+      setTotalPrice(total);
+    });
+  };
+  useEffect(() => {
+    getProductsPrice();
+    return () => {
+      setTotalPrice(0);
+    };
+  }, [cartItems]);
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        <FlatList
+          style={{ backgroundColor: "#F5F5F5" }}
+          data={cartItems}
+          renderItem={({ item }) => <CartItem product={item} />}
+        />
+        <Text style={{ padding: 15, fontWeight: "bold", color: "#5D3EBD" }}>
+          Önerilen Ürünler
+        </Text>
+        <ScrollView
+          style={{ backgroundColor: "white" }}
+          showsHorizontalScrollIndicator={false}
+          bounces={true}
+          horizontal={true}
+        >
+          {Products.map((item, index) => (
+            <ProductItem index={item.id} item={item} />
+          ))}
+        </ScrollView>
+      </ScrollView>
+
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          height: height * 0.12,
+          paddingHorizontal: "4%",
+          width: "100%",
+          backgroundColor: "#f8f8f8",
+          position: "absolute",
+          bottom: 0,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            flex: 3,
+            borderBottomLeftRadius: 8,
+            borderTopLeftRadius: 8,
+            backgroundColor: "#5D3EBD",
+            height: height * 0.06,
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: -10,
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 15, fontWeight: "bold" }}>
+            Devam
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "white",
+            height: height * 0.06,
+            marginTop: -10,
+            borderTopRightRadius: 8,
+            borderBottomRightRadius: 8,
+          }}
+        >
+          <Text
+            style={{
+              color: "#5D3EBD",
+              fontWeight: "bold",
+              fontSize: 15,
+            }}
+          >
+            <Text>{"\u20BA"}</Text>
+            {totalPrice.toFixed(2)}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default index;
